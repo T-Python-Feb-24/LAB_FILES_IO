601bce325841fe77d74779e500f36ff0feee9897
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-13 21:58:28 +0300

    improved even more.

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 def addToDO():
+    """this function will add items to the To-Do list"""
+    file = open("to-do.txt", "a", encoding = "utf-8")
+    list_input = input("please enter the item you want to add: ")
+    file.write(list_input + "\n")
+    file.close()
+
+def showList():
+    """this function will show the items in To-Do list"""
+    file = open("to-do.txt", "r", encoding = "utf-8")
+    todo_list = file.read()
+    print(todo_list)
+    file.close()
+
+def main():
     while True:
-        userInput = input("do you want to add a new To-Do item? if yes enter (Y) if no enter (N), and (exit) to exit the program: ")
-        if userInput.upper() == "Y":
-            file = open("to-do.txt", "a", encoding = "utf-8")
-            list_input = input("please enter your to-do list: ")
-            file.write(list_input + "\n")
-            file.close()
-        
-        elif userInput.upper() == "N":
-            showList()
+        print("type 1 to add an item to the list")
+        print("type 2 to show the items in the list")
+        print("type exit to terminate the programe")
+        userInput = input("Choice: ")
+        if userInput.upper() == "1":
+            print(addToDO())
+            
+
+        elif userInput.upper() == "2":
+            print("type 1 to show the list")
+            print("type 2 to go back to the first choise")
+            refuse = input("Choice: ")
+            if refuse.upper() == "1":
+                 print(showList())
+            
+            elif refuse.upper() == "2":
+                print("Good bye.")
+
+            else:
+                print("please use one of the options.")
+
         
         elif userInput.lower() == "exit":
             break
 
         else:
-            print("please use one of the options (Y), (N) or (exit).")
+            print("please use one of the options (1), (2) or (exit).")
 
-def showList():
-    
-    refuse = input("do you want to list your To-Do items ?if yes enter (Y) if no enter (N): ")
-    if refuse.upper() == "Y":
-        file = open("to-do.txt", "r", encoding = "utf-8")
-        todo_list = file.read()
-        print(todo_list)
-        file.close()
-
-    elif refuse.upper() == "N":
-        print("Good bye.")
-
-    else:
-        print("please use one of the options (Y) or (N).")
-
-def main():
-    try:
-        print(addToDO())
-    except FileNotFoundError:
-        print("the file does not exist.")
 print(main())
