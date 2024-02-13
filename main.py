da00b0deda82288697334d33b09f4f5a1c5a9139
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-13 14:30:28 +0300

    improved on the main code.

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ while True:
         list_input = input("please enter your to-do list: ")
         file.write(list_input + "\n")
         file.close()
-    elif user_input.lower() == "exit":
-        print("See you later.")
-        break
-    else:
+
+    elif user_input.upper() == "N":
         refuse = input("do you want to list your To-Do items ?if yes enter (Y) if no enter (N): ")
         if refuse.upper() == "Y":
             file = open("to-do.txt", "r", encoding = "utf-8")
             todo_list = file.read()
             print(todo_list)
-        else:
+            file.close()
+
+        elif refuse.upper() == "N":
             print("Good bye.")
+
+        else:
+            print("please use one of the options (Y) or (N).")
+
+    elif user_input.lower() == "exit":
+        print("See you later.")
+        break
+
+    else:
+        print("please use one of the options (Y), (N) or (exit).")
